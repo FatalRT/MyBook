3095e2fa6213fe73f481269b40b95a266a94caab
陶成军 <1972033837@qq.com>
2019-01-14 18:29:00 +0800

    测试

--- app/src/main/java/com/example/taoch/mybook/HomeActivity.java
+++ app/src/main/java/com/example/taoch/mybook/HomeActivity.java
@@ -5,11 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hitomi.cmlibrary.CircleMenu;
